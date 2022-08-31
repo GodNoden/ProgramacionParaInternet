1861afa8259670f5c3c4d69cf9df5f95fdf0feb7
GodNoden <noe.quezada7446@alumnos.udg.mx>
2022-08-31 18:22:06 -0500

    first commit

---
+++ LOGIN.js
@@ -0,0 +1,53 @@
+import React from "react";
+import { Button, StyleSheet, TextInput } from "react-native";
+
+import { Component } from 'react';
+import { View, Text, Image } from 'react-native';
+
+export default class Login extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    };
+  }
+
+  render() {
+    return (
+      <View>
+        <Text style={styles.textoudg}> UDG </Text>
+        <Image style={styles.logo} source={require("./Images/udg.png")} />
+        <TextInput style={styles.input} placeholder="Codigo"></TextInput>
+        <TextInput style={styles.input} placeholder="NIP"></TextInput>
+        <View style={styles.button}>
+          <Button title="Entrar"></Button>
+        </View>
+      </View>
+    );
+  }
+
+}
+
+const styles = StyleSheet.create({
+  button: {
+    width: 100,
+    marginTop: 20,
+    marginLeft: 140,
+  },
+  input: {
+    borderWidth: 2,
+    fontSize: 30,
+    marginTop: 20
+  },
+  logo: {
+    width: 200,
+    height: 300,
+    resizeMode: "center",
+    marginLeft: 110,
+  },
+  textoudg: {
+    fontSize: 30,
+    color: "orange",
+    textAlign: "center",
+  },
+});
